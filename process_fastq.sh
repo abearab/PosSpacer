2e3c4fdaf5080681ad8f21b114acdee2bcba5b77
Abolfazl (Abe) <53412130+abearab@users.noreply.github.com>
2022-09-28 16:18:59 -0700

    Update process_fastq.sh

--- process_fastq.sh
+++ process_fastq.sh
@@ -1,34 +1,38 @@
 fastqDIR=$1
-sample=$2
-adaptor5p=$3
-adaptor3p=$4
-length=$5
-Jobs=$6
+logDIR=$2
+sample=$3
+adaptor5p=$4
+adaptor3p=$5
+length=$6
+Jobs=$7
 
 mkdir -p ${fastqDIR}
-mkdir -p logs
-mkdir -p logs/cutadapt
-mkdir -p logs/pear/
+mkdir -p ${logDIR}
+mkdir -p ${logDIR}/cutadapt
+mkdir -p ${logDIR}/pear/
 
-echo -e `date`"\t"$sample"\tassemble"
+
+echo -e `date`"\t"$sample"\tassemble";
+
+
 pear -j $Jobs -q 10 -n ${length} \
     -f ${fastqDIR}/${sample}_R1.fastq.gz \
     -r ${fastqDIR}/${sample}_R2.fastq.gz \
-    -o ${fastqDIR}/${sample} &> logs/pear/${sample}.log.txt;
+    -o ${fastqDIR}/${sample} &> ${logDIR}/pear/${sample}.log.txt;
 wait;
-
 gzip -f ${fastqDIR}/${sample}.*.fastq
+seqkit stats -j $Jobs ${fastqDIR}/${sample}.assembled.fastq.gz -T
 
-echo -e `date`"\t"$sample"\ttrim"
+
+echo -e `date`"\t"$sample"\ttrim";
+
 cutadapt \
-    -j $Jobs \
+    -j ${Jobs} \
     --discard-untrimmed \
     --action "trim" \
-    -a $adaptor5p";e=0.0001;required..."$adaptor3p";e=0.0001;required" \
+    -a $adaptor5p";e=0.0001;required;..."$adaptor3p";e=0.0001;required" \
     -o ${fastqDIR}/${sample}.assembled.trim.fastq.gz \
     ${fastqDIR}/${sample}.assembled.fastq.gz \
-    &> logs/cutadapt/${sample}.log.txt;
+    &> ${logDIR}/cutadapt/${sample}.log.txt;
 wait;
-
-echo -e `date`"\t"$sample"\tprocessed stats"
-seqkit stats ${fastqDIR}/${sample}.processed.fastq.gz -T
+seqkit stats -j $Jobs ${fastqDIR}/${sample}.assembled.trim.fastq.gz -T
